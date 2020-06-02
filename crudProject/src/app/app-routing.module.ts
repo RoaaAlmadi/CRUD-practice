3f7c5150c7b7001c94a373401c30bda81b426922
Gervan <gervan@users.noreply.github.com>
2020-06-02 15:35:58 -0400

    added routing

--- crudProject/src/app/app-routing.module.ts
+++ crudProject/src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from  '../app/home/home.component';
+import { EditUserComponent } from '../app/edit-user/edit-user.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { AvatarDialogComponent } from './avatar-dialog/avatar-dialog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch:'full'
+  },
+  {
+      path: '**', 
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }, 
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'edit-user',
+    component: EditUserComponent
+  },
+  {
+    path:'new-user',
+    component: NewUserComponent
+  },
+  {
+    path:'avatar',
+    component: AvatarDialogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
