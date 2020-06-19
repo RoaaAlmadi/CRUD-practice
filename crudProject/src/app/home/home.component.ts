8b48860d101093c08830d4013b50024524df5303
burningman2k20 <burningman2k9@gmail.com>
2020-06-19 13:47:17 -0400

    something

--- crudProject/src/app/home/home.component.ts
+++ crudProject/src/app/home/home.component.ts
@@ -5,43 +5,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   users: Users[];
   editState: boolean = false;
   userToEdit: Users;
 
-  constructor(public firebaseService: FirebaseService) { }
+  constructor(public firebaseService: FirebaseService) {}
 
   ngOnInit(): void {
-   this.showlist()
-   
+    this.showlist();
   }
-  
-  showlist(){
-    this.firebaseService.getUsers().subscribe(data =>{
-   
-      this.users = data.map(res =>{
-        return{
+
+  updateUser(user: Users) {}
+
+  showlist() {
+    this.firebaseService.getUsers().subscribe((data) => {
+      this.users = data.map((res) => {
+        return {
           id: res.payload.doc.id,
           name: res.payload.doc.data()['name'],
           surname: res.payload.doc.data()['surname'],
           age: res.payload.doc.data()['age'],
-          avatar: res.payload.doc.data()['avatar']
-       }
-      })
-    })
-
+          avatar: res.payload.doc.data()['avatar'],
+        };
+      });
+    });
   }
 
-  deleteUser(user: Users){
+  deleteUser(user: Users) {
     this.firebaseService.deleteUser(user.id);
   }
 
-  editUser(user: Users){
+  editUser(user: Users) {
     this.editState = true;
     this.userToEdit = user;
   }
-
 }
